dd68e51265e48fad0c41e1c7f91fad5b54bfd010
Adilson Junior <asc.juninho@hotmail.com>
2021-03-06 19:11:41 -0300

    Adiciona layout dos componentes 'login' e 'new-user'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './user/login/login.component';
 
 const routes: Routes = [
   {
