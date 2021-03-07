49d3cb7a379d25bb56593aa6cdd1d190890bc01d
Adilson Junior <asc.juninho@hotmail.com>
2021-03-06 22:52:41 -0300

    Adiciona implementaçao do componente 'login'

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {UserService} from '@src/app/user/shared/user.service';
+import { User } from '../shared/model/user.model';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { 
+    this.user = new User();
+  }
 
   ngOnInit(): void {
   }
 
+  handleLoginSubmit() {
+    const credentials = {
+      email: this.user.email,
+      password: this.user.password
+    };
+
+    this.userService.logIn(credentials).subscribe(user => {
+      if(user) {
+        alert(`Bem vindo, ${user.firstName}!`);
+        this.router.navigate(['/main/home']);
+        return;
+      }
+
+      alert("Não existe");
+    }, () => {
+      alert("Erro na conexão");
+    })
+
+    this.user = new User();
+  }
 }
