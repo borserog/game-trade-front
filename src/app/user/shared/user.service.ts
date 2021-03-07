57d768c4a801a780fc9014f2ffdbe4e8e7c7a2c5
Adilson Junior <asc.juninho@hotmail.com>
2021-03-06 22:49:34 -0300

    Adiciona service para o model User

--- src/app/user/shared/user.service.ts
+++ src/app/user/shared/user.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@angular/core';
 import { environment } from '@src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { User } from '@src/app/user/shared/model/user.model';
-import { filter } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+interface LoginData {
+  email: string;
+  password: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +20,19 @@ export class UserService {
     private httpClient: HttpClient
   ) { }
 
-  login(email: string, password: string): any {
-    return this.httpClient.get<User>(this.RESOURCE_URL, { params: { email } }).pipe(
-      filter(user => user.password === password)
+  logIn(credentials: LoginData): Observable<User> {
+    const { email, password } = credentials;
+    return this.httpClient.get<User[]>(this.RESOURCE_URL, { params: { email, password } }).pipe(
+      map(response => response[0])
     );
+
+    // return this.httpClient.post<User>(this.RESOURCE_URL,  credentials).pipe(
+    //   tap(console.log),
+    //   map(response => response[0])
+    // );
+  }
+
+  signUp(user: User): Observable<User> {
+    return this.httpClient.post<User>(this.RESOURCE_URL, user);
   }
 }
