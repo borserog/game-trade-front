1acefd975885792718ec86956457d3c77981a44e
Adilson Junior <asc.juninho@hotmail.com>
2021-03-06 22:54:33 -0300

    Realiza ajustes no app-routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'welcome/login',
+    pathMatch: 'full'
+  },
+  {
     path: 'welcome',
     loadChildren: () => import('./user/user.module').then(m => m.UserModule)
   },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+    path: 'main',
+    loadChildren: () => import('./main/main.module').then(m => m.MainModule)
   }
 ];
 
