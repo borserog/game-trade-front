1a6b23a22da8ab43d9b252149a432ce2b79a3206
Adilson Junior <asc.juninho@hotmail.com>
2021-04-26 19:00:27 -0300

    Adiciona formulário para edição dos dados de usuário

---
+++ src/app/main/user/profile/edit/edit.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@src/app/core/auth.service';
+import { User } from '@src/app/main/user/shared/model/user.model';
+import {UserService} from "@src/app/main/user/shared/service/user.service";
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.scss']
+})
+export class EditComponent implements OnInit {
+  user: User;
+
+  constructor(
+    private authService: AuthService,
+    private userService: UserService
+  ) {
+    this.authService.loggedUser$.subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  handleUpdateSubmit(): void {
+    this.userService.updateUser(this.user);
+  }
+}
